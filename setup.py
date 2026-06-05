a5017c6c712aea1ce4fcbfd8ef74da55989f4d7e
mohit <mohit20041808@gmail.com>
2026-06-05 20:17:25 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Mohit Sharma",
+    author_email="mohit20041808@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
